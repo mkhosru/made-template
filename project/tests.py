17bcee16198ef8ff29f49ac452042231591bebb3
mkhosru <16103010@iubat.edu>
2024-12-07 12:06:21 -0500

    updated_project_feedback_CI_workflow_

--- project/tests.py
+++ project/tests.py
@@ -57,15 +57,32 @@ class TestDataPipeline(unittest.TestCase):
 
         # Assert that 'Year' and 'Value' columns exist in health expenditure data
         self.assertTrue(
-            'Year' in health_df.columns and 'Value' in health_df.columns,
-            "Health expenditure data reshaping failed. 'Year' and 'Value' columns missing."
-        )
+        'Year' in health_df.columns and 'Value' in health_df.columns,
+        "Health expenditure data reshaping failed. 'Year' and 'Value' columns missing."
+    )
+        print("Test success: 'Year' and 'Value' columns found in health expenditure data.")
 
         # Assert that 'Year' and 'Value' columns exist in GDP growth data
         self.assertTrue(
-            'Year' in gdp_df.columns and 'Value' in gdp_df.columns,
-            "GDP growth data reshaping failed. 'Year' and 'Value' columns missing."
-        )
+        'Year' in gdp_df.columns and 'Value' in gdp_df.columns,
+        "GDP growth data reshaping failed. 'Year' and 'Value' columns missing."
+    )
+        print("Test success: 'Year' and 'Value' columns found in GDP growth data.")
+
+        # Test for missing values in health expenditure data
+        self.assertTrue(
+        not health_df.isnull().any().any(),
+        "Health expenditure data contains missing values."
+    )
+        print("Test success: No missing values found in health expenditure data.")
+
+        # Test for missing values in GDP growth data
+        self.assertTrue(
+        not gdp_df.isnull().any().any(),
+        "GDP growth data contains missing values."
+    )
+    print("Test success: No missing values found in GDP growth data.")
+
 
     def test_database_exist(self):
         """Test if database exists after pipeline execution."""
